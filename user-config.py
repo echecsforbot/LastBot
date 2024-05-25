696c970f22bf5c05b7cec01f0dfea822f983de59
echecsforbot <baptistebcsns@tutanota.com>
2024-05-25 20:09:18 +0200

    Update user-config.py

--- user-config.py
+++ user-config.py
@@ -1,4 +1,7 @@
+import os
+
 family = 'wikipedia'
 mylang = 'fr'
 usernames['wikipedia']['fr'] = 'LastBot'
-authenticate['fr.wikipedia.org'] = 'user-password.py'
+authenticate['fr.wikipedia.org'] = (os.environ.get('PWB_CONSUMER_TOKEN'), os.environ.get('PWB_CONSUMER_SECRET'), os.environ.get('PWB_ACCESS_TOKEN'), os.environ.get('PWB_ACCESS_SECRET'))
+
